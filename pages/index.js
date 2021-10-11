7593215104e591001fc5a2186af28d8771415873
Douglas Prado <douglas@currall.com.br>
2021-10-11 15:26:34 -0300

    impl. balanço

--- pages/index.js
+++ pages/index.js
@@ -3,25 +3,21 @@ import Head from 'next/head'
 
 export default function Home() {
   const [ categories, setCategories ] = useState([])
-  const [ balance, setBalance ] = useState([])
-  function currencyFormat(currency, num) {
-    if(currency === 'BRL'){
-      const number = num.toFixed(2).replace('.', ',')
-      return 'R$ ' + number.replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1.')
-    }
-    if (currency === 'USD') {
-      return 'U$ ' + num.toFixed(2).replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
-    }
-  }
+  const [ balance, setBalance ] = useState({})
+  
   useEffect(() => {
+    fetch("http://localhost:8000/api/balance/?format=json")
+      .then(res => res.json())
+      .then(({ balance }) => {
+        setBalance(balance)
+      })
+      .catch(function(error) {  
+        console.log('Request failed', error)  
+      });
     fetch("https://wallid.herokuapp.com/api/categories/?format=json")
       .then(res => res.json())
       .then((data) => {
-        console.log(data)
         setCategories(data);
-        setBalance({
-          balance: "R$ 11.000,00"
-        })
       })
       .catch(function(error) {  
         console.log('Request failed', error)  
@@ -39,7 +35,7 @@ export default function Home() {
         <main className="flex flex-col items-center justify-around w-full flex-1 text-center">
           <div className="flex items-stretch">
             <div className="box-border h-52 w-52 rounded-full shadow-md py-20 border-4 border-gray-50 m-10">
-              <span className="text-2xl font-semibold uppercase text-gray-500">{balance.balance}</span> <br/>
+              <span className="text-2xl font-semibold uppercase text-gray-500">{balance.BRL}</span> <br/>
               <span className="text-sm text-gray-500">
                 <div className="flex items-center justify-center">
                   <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
@@ -52,18 +48,17 @@ export default function Home() {
               </span> 
             </div>
           </div>
-          <div class="container">
+          <div className="container">
             <ul className="px-5 mb-20">
-            { categories.map((category) => {
-              if(category.balance_total > 0){
+            { categories.map((category, index) => {
+              if(category.get_balance_total != "0"){
                 return (
-                <li x-for="item in items" class="rounded-lg shadow-md border border-gray-50 mt-5">
-                <div class="grid grid-flow-col grid-rows-1 grid-cols-3">
+                <li key={index} x-for="item in items" className="rounded-lg shadow-md border border-gray-50 mt-5">
+                <div className="grid grid-flow-col grid-rows-1 grid-cols-3">
                   <div className="flex flex-wrap content-center justify-center">
                     <p className="text-lime-600 dark:text-lime-400 text-xs sm:text-base lg:text-sm xl:text-base font-semibold uppercase text-gray-600">
                       {category.name}
                     </p>
-                    
                   </div>
                   <div className="text-lime-600 dark:text-lime-400 text-sm sm:text-base lg:text-sm xl:text-base uppercase text-xl text-gray-500 p-8 col-span-2">
                   { category.get_balance_total }
