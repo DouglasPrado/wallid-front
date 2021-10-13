8c6131a01a811d5319afb234bd93fe85bb9582f4
Douglas Prado <douglas@currall.com.br>
2021-10-13 12:22:16 -0300

    atualização de cores

--- pages/index.js
+++ pages/index.js
@@ -34,9 +34,9 @@ export default function Home() {
 
         <main className="flex flex-col items-center justify-around w-full flex-1 text-center">
           <div className="flex items-stretch">
-            <div className="box-border h-52 w-52 rounded-full shadow-md py-20 border-4 border-gray-50 m-10">
+            <div className="box-border h-52 w-52 rounded-full shadow-lg py-20 border-4 border-indigo-500 m-10">
               <span className="text-2xl font-semibold uppercase text-gray-500">{balance.total}</span> <br/>
-              <span className="text-sm text-gray-500">
+              <span className="text-sm text-gray-800">
                 <div className="flex items-center justify-center">
                   <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={1} d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z" />
@@ -56,7 +56,7 @@ export default function Home() {
                 <li key={index} x-for="item in items" className="rounded-lg shadow-md border border-gray-50 mt-5">
                 <div className="grid grid-flow-col grid-rows-1 grid-cols-3">
                   <div className="flex flex-wrap content-center justify-center">
-                    <p className="text-lime-600 dark:text-lime-400 text-xs sm:text-base lg:text-sm xl:text-base font-semibold uppercase text-gray-600">
+                    <p className="text-gray-800 dark:text-gray-400 text-xs sm:text-base lg:text-sm xl:text-base font-semibold uppercase text-gray-800">
                       {category.name}
                     </p>
                   </div>
