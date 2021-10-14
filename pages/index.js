d3acce0fc802f11f0a7ea3b76537f3a05c55abbb
Douglas Prado <douglas@currall.com.br>
2021-10-14 14:53:20 -0300

    atualização

--- pages/index.js
+++ pages/index.js
@@ -23,7 +23,7 @@ export default function Home({ categories, balance }) {
           <div className="flex items-stretch">
             <div className="box-border h-52 w-52 rounded-full shadow-lg py-20 border-4 border-gray-500 m-10">
               <span className={`text-2xl font-semibold uppercase text-gray-500 ${blur}`} >{balance.total}</span> <br/>
-              <span className="text-sm text-gray-500">
+              <span className="text-sm text-gray-500 px-2">
                 <div className="flex items-center justify-center">
                   <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
