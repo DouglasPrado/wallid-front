93bde4ba159a07c8c889c9aed24a1e989d497360
Douglas Prado <douglas@currall.com.br>
2021-10-19 16:10:34 -0300

    inserir menu rapido

---
+++ components/Navbar.js
@@ -0,0 +1,36 @@
+
+const Navbar = ({navigations}) => {
+  return (
+    <>
+      <div class="flex overflow-x-scroll pb-10 hide-scroll-bar">
+        <div class="flex flex-nowrap lg:ml-3 md:ml-3 ml-3 ">
+          {navigations.map(navigation => (
+            <div class="inline-block px-3">
+            <div class="w-32 h-24 max-w-xs overflow-hidden rounded-lg shadow-md border border-gray-100 bg-white hover:shadow-xl transition-shadow duration-300 ease-in-out">
+              <div className="grid m-3 ">
+                <div className="col-span-3 mb-5">
+                  {navigation.icon}
+                </div>
+                <div className="">
+                  <h1 className="text-sm text-left font-light uppercase text-gray-500">{navigation.title}</h1>
+                </div>
+              </div>
+            </div>
+          </div>
+          ))}   
+        </div>
+      </div>
+      <style>{`
+        .hide-scroll-bar {
+          -ms-overflow-style: none!important;
+          scrollbar-width: none!important;
+        }
+        .hide-scroll-bar::-webkit-scrollbar {
+          display: none!important;
+        }
+    `}</style>
+  </>
+  );
+}
+
+export default Navbar;
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,35 @@
 import React, { useState, useEffect, useCallback } from 'react'
 import Head from 'next/head'
+import Navbar from '../components/Navbar'
 
 export default function Home({ categories, balance }) {
   const [blur, setBlur] = useState(`blur`)
+  const navigations = [
+    {
+      icon: <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-gray-600" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={1.5} d="M5 8h14M5 8a2 2 0 110-4h14a2 2 0 110 4M5 8v10a2 2 0 002 2h10a2 2 0 002-2V8m-9 4h4" />
+            </svg>,
+      title: 'Minha conta'
+    },
+    {
+      icon: <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-gray-600" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={1.5} d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4" />
+            </svg>,
+      title: 'Aportes'
+    },
+    {
+      icon: <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-gray-600" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={1.5} d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-8l-4-4m0 0L8 8m4-4v12" />
+            </svg>,
+      title: 'Retiradas'
+    },
+    {
+      icon: <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-gray-600" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={1.5} d="M13 7h8m0 0v8m0-8l-8 8-4-4-6 6" />
+            </svg>,
+      title: 'Projeção'
+    }
+  ]
   const clickBlur = useCallback(() => {
     if(!blur) {
       setBlur('blur-lg')
@@ -58,6 +85,9 @@ export default function Home({ categories, balance }) {
               }}
             )}
             </ul>
+          </div>
+          <div className="container">
+            <Navbar navigations={navigations}/>
           </div>
         </main>
       </div>
