0e3b2d4849d71de1dd1595a1119f770667b681f3
Douglas Prado <douglas@currall.com.br>
2021-10-21 16:57:13 -0300

    ajustes de layout

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,7 +2,7 @@
 const Navbar = ({navigations}) => {
   return (
     <>
-      <div className="inset-x-0 bottom-0 absolute mb-10">
+      <div className="inset-x-0 bottom-0 absolute mb-5">
         <h1 className="m-3 text-base uppercase text-gray-600">Acesso rápido</h1>  
         <div className="overflow-x-scroll hide-scroll-bar ">
           <div className="flex flex-nowrap">
